3620f06b5a573bc093982a7377d6722fdd247e75
rotemdan267 <86420467+rotemdan267@users.noreply.github.com>
2021-06-28 03:54:33 +0300

    Add files via upload

---
+++ HW-Lesson-3/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1
+
+            //int[] array = new int[10];
+            //int counter = 0;
+
+            //Console.WriteLine("Please enter 10 values:");
+
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter++] = int.Parse(Console.ReadLine());
+            //array[counter] = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+            //Console.WriteLine(array[counter--]);
+
+
+            #endregion
+
+            #region Q2
+
+            //char[] arr = new char[5];
+
+            //arr[0] = 'a';
+            //arr[1] = 'b';
+            //arr[2] = 'c';
+            //arr[3] = 'd';
+            //arr[4] = 'e';
+
+            //Console.WriteLine(arr[0]);
+            //Console.WriteLine(arr[1]);
+            //Console.WriteLine(arr[2]);
+            //Console.WriteLine(arr[3]);
+            //Console.WriteLine(arr[4]);
+
+            #endregion
+
+            #region Q3
+
+            //int[] arr1 = new int[10];
+            //int[] arr2 = new int[10];
+
+            //Console.WriteLine("Please enter 10 values:");
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //    arr2[i] = arr1[i];
+            //}
+
+            //for(int i = 0; i<arr2.Length;i++)
+            //{
+            //    Console.WriteLine($"arr1[{i}] = {arr1[i]}");
+            //    Console.WriteLine($"arr2[{i}] = {arr2[i]}");
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("Please enter 10 new values for arr1:");
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < arr2.Length; i++)
+            //{
+            //    Console.WriteLine($"arr1[{i}] = {arr1[i]}");
+            //    Console.WriteLine($"arr2[{i}] = {arr2[i]}");
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Q4+Q5
+
+            //int[] arr1 = new int[10];
+            //int[] arr2 = new int[10];
+
+            //Console.WriteLine("Please enter 10 values:");
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //arr2 = arr1;
+
+            //for (int i = 0; i < arr2.Length; i++)
+            //{
+            //    Console.WriteLine($"arr1[{i}] = {arr1[i]}");
+            //    Console.WriteLine($"arr2[{i}] = {arr2[i]}");
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("Please enter 10 new values for arr1:");
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < arr2.Length; i++)
+            //{
+            //    Console.WriteLine($"arr1[{i}] = {arr1[i]}");
+            //    Console.WriteLine($"arr2[{i}] = {arr2[i]}");
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Q6
+
+            //Console.WriteLine("How mant students are there in your class?");
+            //int students = int.Parse(Console.ReadLine());
+
+            //int[] grades = new int[students];
+
+            //Console.WriteLine("Please enter the students' grades:");
+            //for (int i = 0; i < students; i++)
+            //{
+            //    grades[i] = int.Parse(Console.ReadLine());
+            //}
+            //int sum = 0;
+
+            //for (int i = 0; i < grades.Length; i++)
+            //{
+            //    sum += grades[i];
+            //}
+            //float avg = 0;
+            //if (students != 0)
+            //{
+            //    avg = (float)sum / students;
+            //    Console.WriteLine($"The grades' sum is: {sum} and the avergae is: {avg}");
+            //}
+
+            #endregion
+
+        }
+    }
+}
